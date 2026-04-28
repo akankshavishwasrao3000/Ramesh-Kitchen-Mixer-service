6536059d8428ce2beb0f8f9caea87638d245ea41
Akanksha Vishwasrao <akankshavishwasrao3000@gmail.com>
2026-04-28 21:55:31 +0530

    added changes in ALLOWED_HOSTS

--- home/settings.py
+++ home/settings.py
@@ -35,7 +35,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'ramesh-kitchen-mixer-service-production.up.railway.app'
+]
 
 
 # Application definition
