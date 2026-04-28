3e2973a37c349a956cd4de294e6b27597f2b23ab
Akanksha Vishwasrao <akankshavishwasrao3000@gmail.com>
2026-04-28 22:21:30 +0530

    added changes in settings

--- home/settings.py
+++ home/settings.py
@@ -115,11 +115,12 @@ if os.getenv("DATABASE_URL"):
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+        'default': dj_database_url.config(
+            default='sqlite:///db.sqlite3'
+        )
     }
+
+    
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
