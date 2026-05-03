6a6875b5d0a0fe47c68f9bbcb56f37a89e1a2f24
Akanksha Vishwasrao <akankshavishwasrao3000@gmail.com>
2026-05-03 16:25:07 +0530

    added cloudinary setup

--- home/settings.py
+++ home/settings.py
@@ -139,6 +139,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+import cloudinary
+
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
